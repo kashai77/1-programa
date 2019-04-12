a9ba9c086dffe3954928ac2588cede435c75fa30
kashai <nghebuta@gmail.com>
2019-04-12 19:48:37 +0100

    elegir el mayor numero

---
+++ el_mayor_de_3.py
@@ -0,0 +1,7 @@
+primer_numero = int(input("primer numero "))
+segundo_numero = int(input("segundo numero "))
+tercer_numero = int(input("tercer numero "))
+
+max_number = max(primer_numero, segundo_numero, tercer_numero)
+
+print ("el mayor es {}".format(max_number))
