49b0edac0ac817e219528539e9a1d84ac10c8ff2
kashai <nghebuta@gmail.com>
2019-04-12 20:48:40 +0100

    IF/ELSE

---
+++ if/else.py
@@ -0,0 +1,17 @@
+
+
+#user_number = int(input("Escribe un numero: "))
+
+#if user_number > 5:
+    #print("+ que 5")
+#else:
+    #print("- que 5")
+
+number_to_gues = 2
+
+user_number = int(input("Adivina el numero: "))
+
+if number_to_gues == user_number:
+    print("adivinaste")
+else:
+    print("fallaste")
